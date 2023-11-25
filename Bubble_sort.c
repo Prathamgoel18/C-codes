4fa0e4651364a0256aad02c6a67e09941a5c2a76
Prathamgoel18 <147034150+Prathamgoel18@users.noreply.github.com>
2023-11-25 12:39:33 +0530

    Add files via upload

---
+++ Bubble_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+void bubbleSort(int arr[100], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+            
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void printArray(int arr[100], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Unsorted array: ");
+    printArray(arr, n);
+
+    
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
